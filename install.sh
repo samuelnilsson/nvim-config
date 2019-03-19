2546d64fcab161c975f3d4a7b4dc2f847b048cda
Samuel Nilsson <samni698@gmail.com>
2019-03-19 19:38:10 +0100

    Fixed stylelintrc default config path issue.

--- install.sh
+++ install.sh
@@ -12,6 +12,26 @@ link_file()
 	fi
 }
 
+copy_file()
+{
+	file=$1
+	copy=$2
+	copied=false
+	if [ -L "$copy" ] || [ -f "$copy" ]
+	then
+		echo "Skipped copying $copy since it already exists."
+	else
+		cp "$file" "$copy"
+		copied=true
+	fi
+}
+
 # Link linter default configurations
-link_file $BASEDIR/langconfigs/stylelintrc.json $HOME/.stylelintrc
 link_file $BASEDIR/langconfigs/eslintrc.json $HOME/.eslintrc.json
+copy_file $BASEDIR/langconfigs/stylelintrc.json $HOME/.stylelintrc.json
+if [ "$copied" = true ]
+then
+	npmdir="$(npm root -g)"
+	jq -c ".extends = \"$npmdir/stylelint-config-recommended\"" $HOME/.stylelintrc.json > tmp.$$.json
+	mv tmp.$$.json $HOME/.stylelintrc.json
+fi

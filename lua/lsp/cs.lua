0929c105b562ae4695d1eb14196b5cf17f85dd5c
Samuel Nilsson <samuel.nilsson@voyado.com>
2021-08-17 15:10:24 +0200

    use environment variable to determine omnisharp location

--- lua/lsp/cs.lua
+++ lua/lsp/cs.lua
@@ -2,8 +2,7 @@ local lsp_config = require('lspconfig')
 local on_attach = require('lsp.on_attach')
 
 local pid = vim.fn.getpid()
-local home = os.getenv('HOME')
-local omnisharp_bin = (home .. '/scoop/apps/omnisharp/current/OmniSharp.exe')
+local omnisharp_bin = (os.getenv('OMNISHARP_DIRECTORY') .. '/OmniSharp.exe')
 
 lsp_config.omnisharp.setup{
     cmd = { omnisharp_bin, '--languageserver' , "--hostPID", tostring(pid) };

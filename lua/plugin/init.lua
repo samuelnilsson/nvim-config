a4756cfe961990750e3c52eb62949525445e6ac9
Samuel Nilsson <samni698@gmail.com>
2021-10-28 15:01:58 +0200

    Add coc icons. Remove session manager. Use telescope git_files instead of files.

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -2,92 +2,110 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({
-        'git', 'clone', 'https://github.com/wbthomason/packer.nvim',
-        install_path
-    })
-    execute 'packadd packer.nvim'
+	fn.system({
+		"git",
+		"clone",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	execute("packadd packer.nvim")
 end
 
 -- Packages
-return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+	use("wbthomason/packer.nvim")
 
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate',
-        config = function() require('plugin.treesitter') end
-    }
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+		config = function()
+			require("plugin.treesitter")
+		end,
+	})
 
-	use {
-		'neoclide/coc.nvim',
-		branch = 'release',
-        config = function() require('plugin.coc') end
-	}
+	use({
+		"neoclide/coc.nvim",
+		branch = "release",
+		config = function()
+			require("plugin.coc")
+		end,
+	})
 
-    use 'navarasu/onedark.nvim'
+	use("navarasu/onedark.nvim")
 
-    use {
-        'nvim-telescope/telescope.nvim',
-        requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}},
-        config = function() require('plugin.telescope') end
-    }
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = { { "nvim-lua/popup.nvim" }, { "nvim-lua/plenary.nvim" } },
+		config = function()
+			require("plugin.telescope")
+		end,
+	})
 
-    use {
-        'lewis6991/gitsigns.nvim',
-        requires = {'nvim-lua/plenary.nvim'},
-        config = function() require('plugin.gitsigns') end
-    }
+	use({
+		"lewis6991/gitsigns.nvim",
+		requires = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("plugin.gitsigns")
+		end,
+	})
 
-    use {
-        'windwp/nvim-autopairs',
-        config = function() require('nvim-autopairs').setup() end
-    }
+	use({
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup()
+		end,
+	})
 
-    use 'dstein64/nvim-scrollview'
+	use("dstein64/nvim-scrollview")
 
-    use {
-        'hoob3rt/lualine.nvim',
-        requires = {'kyazdani42/nvim-web-devicons'},
-        config = function() require('plugin.lualine') end
-    }
+	use({
+		"hoob3rt/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons" },
+		config = function()
+			require("plugin.lualine")
+		end,
+	})
 
-    use {
-        'vimwiki/vimwiki',
-        config = function()
-            -- try to require lua/wiki.lua where vim.g.vimwiki_list is set
-            pcall(require, 'plugin.wiki')
-        end
-    }
+	use({
+		"vimwiki/vimwiki",
+		config = function()
+			-- try to require lua/wiki.lua where vim.g.vimwiki_list is set
+			pcall(require, "plugin.wiki")
+		end,
+	})
 
-    use {
-        'kyazdani42/nvim-tree.lua',
-        requires = {'kyazdani42/nvim-web-devicons'},
-        config = function() require('plugin.nvimtree') end
-    }
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = { "kyazdani42/nvim-web-devicons" },
+		config = function()
+			require("plugin.nvimtree")
+		end,
+	})
 
-    use {
-        'simrat39/symbols-outline.nvim',
-        config = function() require('plugin.symbols_outline') end
-    }
+	use({
+		"simrat39/symbols-outline.nvim",
+		config = function()
+			require("plugin.symbols_outline")
+		end,
+	})
 
-    use {
-        'norcalli/nvim-colorizer.lua',
-        config = function() require('colorizer').setup() end
-    }
+	use({
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup()
+		end,
+	})
 
-    use 'tpope/vim-fugitive'
+	use("tpope/vim-fugitive")
 
-    use 'rmagatti/auto-session'
+	use("b3nj5m1n/kommentary")
 
-    use 'b3nj5m1n/kommentary'
+	use("tpope/vim-dadbod")
+	use("kristijanhusak/vim-dadbod-ui")
+	use("kristijanhusak/vim-dadbod-completion")
 
-    use 'tpope/vim-dadbod'
-    use 'kristijanhusak/vim-dadbod-ui'
-	use 'kristijanhusak/vim-dadbod-completion'
-
-	use 'diepm/vim-rest-console'
+	use("diepm/vim-rest-console")
 end)
--- lua/plugin/telescope.lua
+++ lua/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local map = require("helper").map
 
-map("n", "<Leader>sf", '<cmd>lua require("telescope.builtin").find_files()<CR>')
+map("n", "<Leader>sf", '<cmd>lua require("plugin.telescope_config").project_files()<CR>')
 map("n", "<Leader>sg", '<cmd>lua require("telescope.builtin").live_grep()<CR>')
 map("n", "<Leader>sb", '<cmd>lua require("telescope.builtin").buffers()<CR>')
 map("n", "<Leader>sh", '<cmd>lua require("telescope.builtin").help_tags()<CR>')

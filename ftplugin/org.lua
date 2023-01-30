c27901ee34be32eccd99595fdb5873359e8c898b
{{ (bitwarden item full_name).notes }} <samuel@samuelnilsson.net>
2023-01-30 21:13:01 +0100

    add orgmode formatting shortcut and archive file

---
+++ ftplugin/org.lua
@@ -0,0 +1,3 @@
+local map = require('helper').map
+
+map("n", "<leader>ff", "gggqG")
--- lua/plugin/orgmode.lua
+++ lua/plugin/orgmode.lua
@@ -5,8 +5,10 @@ orgmode.setup {}
 orgmode.setup_ts_grammar()
 
 orgmode.setup({
-    org_agenda_files = {
-        "~/wiki/inbox.org", "~/wiki/gtd.org", "~/wiki/tickler.org"
-    },
-    org_default_notes_file = "~/wiki/inbox.org"
+	org_agenda_files = {
+		"~/wiki/inbox.org", "~/wiki/gtd.org", "~/wiki/tickler.org", "~/wiki/workcal.org", "~/wiki/someday.org"
+	},
+	org_default_notes_file = "~/wiki/inbox.org",
+	org_archive_location = "~/wiki/archive.org",
+	org_todo_keywords = { 'TODO', 'WAITING', '|', 'DONE', 'CANCELLED' }
 })
